a68101a0caeae04eecfe1698af1f8df5ed83dc12
Moses <moses.adelere@gmail.com>
2018-12-31 17:52:24 +0000

    MyDocFile

---
+++ C#/UnitTesting.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MosesNewFrameWork
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        Console.WriteLine('This is just a test')
+
+    }
+    {
+        
+    }
+    
+}
+
+    
